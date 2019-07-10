e984494dbcb1f82fcce556a0061c22f7873c8d5b
Naser Masri <Naser.eng.85@hotmail.com>
2019-07-10 09:34:05 +0200

    add css file

---
+++ src/components/Search.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import Search from './Search.css'
+export default class Search extends Component {
+  render() {
+    return <div>webpack module</div>;
+  }
+}
